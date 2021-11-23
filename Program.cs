028c9440f953976e9682ba62e308ffd73245e1f1
bustedbunny <buster1998@mail.ru>
2021-11-23 13:32:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter number of factorial.");
+string input = Console.ReadLine();
+int N = 0;
+try
+{
+    N = Int32.Parse(input);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Input is not valid number: " + input);
+    return;
+}
+int num = 0;
+while (N > 0)
+{
+    N /= 5;
+    num += N;
+}
+Console.WriteLine(num);
